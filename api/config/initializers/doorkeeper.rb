b080fde6412db8a392def8ae9042f4652c0eecc0
Mouhannad <49256154+MoeAl-khalkhali@users.noreply.github.com>
2022-10-11 23:34:17 +1100

    Update doorkeeper.rb

--- api/config/initializers/doorkeeper.rb
+++ api/config/initializers/doorkeeper.rb
@@ -2,11 +2,15 @@ Doorkeeper.configure do
   orm :active_record
   use_refresh_token
   api_only
-  base_controller 'ActionController::API'
+  base_controller 'Spree::Api::V2::BaseController'
+  base_metal_controller 'Spree::Api::V2::BaseController'
 
+  # FIXME: we should only skip this for Storefront API until v5
+  # we should not skip this for Platform API
   skip_client_authentication_for_password_grant { true } if defined?(skip_client_authentication_for_password_grant)
 
   resource_owner_authenticator { current_spree_user }
+  use_polymorphic_resource_owner
 
   resource_owner_from_credentials do
     user = Spree.user_class.find_for_database_authentication(email: params[:username])
@@ -21,7 +25,7 @@ Doorkeeper.configure do
   end
 
   admin_authenticator do |routes|
-    current_spree_user&.has_spree_role?('admin') || redirect_to(routes.root_url)
+    current_spree_user&.spree_admin? || redirect_to(routes.root_url)
   end
 
   grant_flows %w[password client_credentials]
@@ -33,16 +37,12 @@ Doorkeeper.configure do
   access_token_methods :from_bearer_authorization, :from_access_token_param
 
   optional_scopes :admin, :write, :read
-end
 
-Doorkeeper::AccessGrant.class_eval do
-  self.table_name = 'spree_oauth_access_grants'
-end
+  access_token_class 'Spree::OauthAccessToken'
+  access_grant_class 'Spree::OauthAccessGrant'
+  application_class 'Spree::OauthApplication'
 
-Doorkeeper::AccessToken.class_eval do
-  self.table_name = 'spree_oauth_access_tokens'
-end
-
-Doorkeeper::Application.class_eval do
-  self.table_name = 'spree_oauth_applications'
+  # using Bcrupt for token secrets is currently not supported by Doorkeeper
+  hash_token_secrets fallback: :plain
+  hash_application_secrets fallback: :plain, using: '::Doorkeeper::SecretStoring::BCrypt'
 end
